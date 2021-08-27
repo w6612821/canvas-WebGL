d03a99ebab15380662b461af10d22ce9392268fa
jiawen.wang <jiawen.wang@sdbean.com>
2021-08-27 18:02:51 +0800

    Feat:卡牌反转

--- src/main.js
+++ src/main.js
@@ -1,36 +1,6 @@
 /*
  * @Author: jianwen.Wang
  * @Date: 2021-08-21 15:52:55
- * @LastEditTime: 2021-08-26 17:59:04
+ * @LastEditTime: 2021-08-27 09:21:46
  * @LastEditors: jiawen.wang
  */
-let cardBig = { width: 500, height: 800, imgWidth: 84, imgHeight: 126 }
-let speed = cardBig.imgWidth * 0.1
-let start;
-let mainBack = new WebGL($('#myCanvas')[0], 0, 0, 500, 800, 84, 126, "./img/back.png");
-let mainFront0 = new WebGL($('#myCanvas')[0], 84, 0, 500, 800, 84, 126, "./img/0.png");
-
-mainBack.drawCardBack()
-mainFront0.drawCardFront()
-
-let big = function (timestamp) {
-    if (start === undefined)
-        start = timestamp;
-    const elapsed = timestamp - start;
-
-    //     cardBig.imgWidth = cardBig.imgWidth + Math.min(0.01 * elapsed, 3.48)
-    //     cardBig.imgHeight = cardBig.imgHeight + Math.min(0.01 * elapsed, 5.21)
-
-    //     main.drawCardBack()
-
-    if (elapsed < 1300) { // 在两秒后停止动画
-        window.requestAnimationFrame(big);
-    }
-}
-
-let btn = document.getElementById('big-btn')
-btn.onclick = function () {
-    ctx = $('#myCanvas')[0].getContext('2d');
-    ctx.clearRect(0, 0, 100, 100)
-}
-
--- src/util.js
+++ src/util.js
@@ -1,29 +1,62 @@
 /*
  * @Author: jianwen.Wang
  * @Date: 2021-08-21 15:54:17
- * @LastEditTime: 2021-08-26 17:58:34
+ * @LastEditTime: 2021-08-27 10:54:32
  * @LastEditors: jiawen.wang
  */
-function WebGL(context, x, y, w, h, pw, ph, url) {
-    context.width = w;
-    context.height = h;
+let canvas = document.getElementById('myCanvas')
+canvas.width = 500;
+canvas.height = 500;
+this.ctx = canvas.getContext('2d');
+function WebGL(x, y, w, h, pw, ph, url) {
     this.x = x;
     this.y = y;
     this.url = url;
     this.pw = pw;
     this.ph = ph;
-    this.ctx = context.getContext('2d');
-    // this.ctx.fillStyle = "#000";
-    // this.ctx.fillRect(0, 0, w, h);
+
 }
+let ballArr = []
+WebGL.prototype.drawBall = function () {
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, 500, 500);
+    // ball.drawCircle()
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, 1, Math.PI * 2, false)
+    ctx.roate = Math.PI * 2
+    ctx.fillStyle = 'red';
+    ctx.fill()
+    ballArr.push(this)
+}
+WebGL.prototype.drawCircle = drawCircle
+function drawCircle() {
+    ctx.beginPath()
+    ctx.arc(250, 250, 200, 0, Math.PI * 2 / 9, false)
+    ctx.strokeStyle = 'green';
+    ctx.stroke()
+}
+drawCircle()
+
+// for (let i = 0; i < 9; i++) {
+//     ball.drawCircle()
+
+// }
+// setInterval(() => {
+//     // ctx.clearRect(0, 0, 500, 500);
+//     ball.drawBall()
+
+//     ballArr[0].x += 1
+//     ballArr[0].y += 1
+//     console.log(ballArr[0])
+// }, 100);
+
 
 WebGL.prototype.drawCardBack = function () {
     let backImg = new Image()
     backImg.src = this.url
-    var that = this
     backImg.onload = function () {
         // 执行drawImage语句
-        that.ctx.drawImage(backImg, that.x, that.y, that.pw, that.ph);
+        that.ctx.drawImage("./img/back.png", 50, 50, 84, 126);
         // that.ctx.restore();
     }
 
@@ -31,7 +64,6 @@ WebGL.prototype.drawCardBack = function () {
 WebGL.prototype.drawCardFront = function () {
     let backImg = new Image()
     backImg.src = this.url
-    var that = this
     backImg.onload = function () {
         // 执行drawImage语句
         that.ctx.drawImage(backImg, that.x, that.y, that.pw, that.ph);
